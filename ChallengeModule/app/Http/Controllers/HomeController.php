4ce61ce13ff474937fcf7ad170d842ee982a5858
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2024-02-27 23:09:16 -0500

    HomeController

--- ChallengeModule/app/Http/Controllers/HomeController.php
+++ ChallengeModule/app/Http/Controllers/HomeController.php
@@ -1,14 +1,12 @@
+<?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    /**
-     * Display the home page.
-     *
-     * @return \Illuminate\View\View
-     */
+
     public function index(): View
     {
         return view('home.index');
